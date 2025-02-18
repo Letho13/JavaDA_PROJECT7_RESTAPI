4c7cd867c2dc60f5c003d6aec05ca9f093352534
Theotime.Lebreton <theotime.lebreton@gmail.com>
2025-02-18 11:02:04 +0100

    Creation git

---
+++ src/main/java/com/nnk/springboot/domain/Trade.java
@@ -0,0 +1,50 @@
+package com.nnk.springboot.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "trade")
+public class Trade {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "TradeId")
+    private Integer tradeId;
+
+    @NotBlank(message = "Account is mandatory")
+    private String account;
+
+    @NotBlank(message = "Type is mandatory")
+    private String type;
+
+    private Double buyQuantity;
+    private Double sellQuantity;
+    private Double buyPrice;
+    private Double sellPrice;
+    private String benchmark;
+    private Timestamp tradeDate;
+    private String security;
+    private String status;
+    private String trader;
+    private String book;
+    private String creationName;
+    private Timestamp creationDate;
+    private String revisionName;
+    private Timestamp revisionDate;
+    private String dealName;
+    private String dealType;
+    private String sourceListId;
+    private String side;
+
+
+    // TODO: Map columns in data table TRADE with corresponding java fields
+}
